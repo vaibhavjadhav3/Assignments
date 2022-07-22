6be70fc84c1573d71f7257053f886099f3f6acd1
vaibhavjadhav3 <108786275+vaibhavjadhav3@users.noreply.github.com>
2022-07-22 23:14:39 +0530

    Add files via upload

---
+++ eclipse-workspace/javaproject1/src/Displaymultiplicationmatrix.java
@@ -0,0 +1,38 @@
+
+public class Displaymultiplicationmatrix 
+
+
+{
+    public static void main(String[] args) 
+    
+    {
+	int i;
+	int j;
+	
+	
+	    for(j=1;j<=12;j++)
+	       {
+		{
+	       
+		for(i=1;i<=12;i++)
+		System.out.format("%4d",i*j);
+		}
+		System.out.print('\n');
+		
+	       }
+	    
+	        
+	       
+	            
+		
+    }
+}
+
+	    
+	
+	
+
+    
+    
+
+
